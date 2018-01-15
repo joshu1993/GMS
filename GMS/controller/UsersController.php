38166f331d92176267da6a5601afcb8136c8009d
dominrivera <dominrivera1@gmail.com>
2018-01-15 18:00:41 +0100

    cambio

--- GMS/controller/UsersController.php
+++ GMS/controller/UsersController.php
@@ -1,248 +1,142 @@
 <?php
+require_once("../model/User.php");
+require_once("../model/UserMapper.php");
+require_once("../core/connectionBD.php");
 
-require_once(__DIR__."/../core/ViewManager.php");
-require_once(__DIR__."/../core/I18n.php");
+class UsersController{
 
-require_once(__DIR__."/../model/User.php");
-require_once(__DIR__."/../model/UserMapper.php");
+		private $userMapper;
 
-require_once(__DIR__."/../controller/BaseController.php");
-
-
-class UsersController extends BaseController {
-
-
-	private $userMapper;
-
-	public function __construct() {
-		parent::__construct();
-
-		$this->userMapper = new UserMapper();
-
-	}
-
-	public function index() {
-		if (!$this->userMapper->esAdmin($this->currentUser->getUsername())) {
-			throw new Exception("No eres Admin");
-		}
-
-		$user = $this->userMapper->findAll();
-
-		$this->view->setVariable("users", $user);
-
-		$this->view->render("users", "index");
-	}
-
-
-	public function view(){
-		if (!isset($_GET["nombreusuario"])) {
-			throw new Exception("id is mandatory");
-		}
-
-		$nombreusuario = $_GET["nombreusuario"];
-
-		$user = $this->userMapper->findByName($nombreusuario);
-
-		if ($user == NULL) {
-			throw new Exception("no such user with name: ".$nombreusuario);
-		}
-
-		$this->view->setVariable("user", $user);
-
-		$this->view->render("users", "view");
-
+		public function __construct()
+		{
+			$this->userMapper = new UserMapper();
 	}
 
 //LOGIN
-	public function login() {
-		if (isset($_POST["nombreusuario"])){
-			if ($this->userMapper->isValidUser($_POST["nombreusuario"],$_POST["contraseña"])) {
-
-				$_SESSION["currentuser"]=$_POST["nombreusuario"];
-
-				$this->view->redirect("users", "index");
-
+public static function login(){
+		global $connect;
+		$nombreusuario = $_POST['nombreusuario'];
+		$contraseña = $_POST['contraseña'];
+		if (isset($_GET['lang'])) {
+			$lang = $_GET['lang'];
+		} else {
+			$lang = "es";
+		}
+		if(empty($nombreusuario) || empty($contraseña)){
+			header("Location: ../index.php?lang=$lang");
+			exit();
+		}
+		$consulta = "SELECT * FROM usuario WHERE nombreusuario='". $nombreusuario."'";
+		$resultado = $connect->query($consulta);
+		if($row = mysqli_fetch_assoc($resultado)){
+			if($row['contraseña'] == contraseña){
+			session_start();
+			$_SESSION['nombreusuario'] = $nombreusuario;
+			$_SESSION['tipousuario'] = $row['tipousuario'];
+			header("Location: ../view/adminIndex.php?lang=$lang");
 			}else{
-				$errors = array();
-				$errors["general"] = "Nombre de usuario no válido";
-				$this->view->setVariable("errors", $errors);
+				header("Location: ../index.php?lang=$lang");
+				exit();
 			}
+		}else{
+			header("Location: ../index.php?lang=$lang");
+			exit();
+		}
+	}
+
+	public static function logout(){
+			if(!isset($_SESSION)) session_start();
+			session_destroy();
+			if (isset($_SESSION['lang'])) {
+				$lang = $_SESSION['lang'];
+			} else {
+				$lang = "es";
+			}
+			header("Location: ../index.php?lang=$lang");
 		}
 
-		$this->view->render("users", "login");
-	}
+		public static function getcurrentUser($nombreusuario){
+			return User::getbyUsername($nombreusuario);
+		}
+
+		public function listarEntrenadores()
+		{
+			return $this->usuarioMapper->listarEntrenadores();
+		}
+
+		public static function searchByUsername($nombreusuario){
+			return $this->userMapper->findByUsername($nombreusuario);
+		}
 
 //REGISTRO
-	public function register() {
-
-		$user = new User();
-
-		if (isset($_POST["submit"])){
-
-			$user->setUsername($_POST["nombreusuario"]);
-			$user->setPassword($_POST["contraseña"]);
-			$user->setMail($_POST["correo"]);
-			$user->setUserType($_POST["tipousuario"]);
-
-			try{
-				$user->checkIsValidForRegister();
-
-
-					if (!$this->userMapper->usernameExists($_POST["nombreusuario"])){
-
-					$this->userMapper->save($user);
-
-
-					$this->view->setFlash(sprintf(i18n("usuario\"%s\" añadido"),$user->getUsername()));
-
-
-					$this->view->redirect("users", "login");
-				} else {
-					$errors = array();
-					$errors["nombreusuario"] = "El nombre de usuario ya existe";
-					$this->view->setVariable("errors", $errors);
-				}
-			}catch(ValidationException $ex) {
-
-				$errors = $ex->getErrors();
-
-				$this->view->setVariable("errors", $errors);
+public static function register()
+		{
+    		$userMapper = new UserMapper();
+    		if (isset($_GET['lang'])) {
+				$lang = $_GET['lang'];
+			} else {
+				$lang = "es";
 			}
-		}
 
+		    if(isset($_POST["nombreusuario"]))
+		    {
 
-		$this->view->setVariable("user", $user);
+			    $user = new User($_POST["nombreusuario"],$_POST["nombre"],$_POST["contraseña"],$_POST["correo"],"entrenador");
 
-		$this->view->render("users", "register");
+			     	try{
+			     		if (!$userMapper->usernameExists($user->getUsername()))
+			     		{
+				      		$userMapper->save($user);
+				      		header("Location: ../view/adminEntrenadores.php?lang=$lang");
+						}
+						else
+						{
+							echo '<script language="javascript">alert("'.__('Ya existe un entrenador con el mismo nombre.',$lang).'"); window.location.href="../view/adminEntrenadores.php?lang=$lang";</script>';
+				      	}
 
-	}
-	public function logout() {
-		session_destroy();
-
-
-		$this->view->redirect("users", "login");
-
-	}
-
-	public function perfil() {
-
-
-		if (isset($_POST["submit"])) {
-
-			$user->setPassword($_POST["contraseña"]);
-			$user->setMail($_POST["correo"]);
-			$user->setUserType($_POST["tipousuario"]);
-
-
-			try {
-
-				$user->checkIsValidForUpdate();
-
-
-				$this->userMapper->update($user);
-
-
-				$this->view->redirect("users", "index");
-
-			}catch(ValidationException $ex) {
-
-				$errors = $ex->getErrors();
-
-				$this->view->setVariable("errors", $errors);
+		      	}catch(ValidationException $ex)
+		      	{
+		      		$errors = $ex->getErrors();
+		     	}
 			}
-		}
-
-
-		$this->view->setVariable("user", $user);
-
-		$this->view->render("users", "perfil");
-
-
 		}
 
 
 
 	public function edit() {
-		if (!isset($_REQUEST["nombreusuario"])) {
-			throw new Exception("A username is mandatory");
-		}
+			$usuarioMapper = new UserMapper();
+			$nombreusuario = $_POST["nombreusuario"];
+			$nombre = $_POST["nombre"];
+			$nombreadmin = $_POST["nombreAdmin"];
+			$nombreAntiguo = $_POST["nombreAntiguo"];
+			$contraseña = $_POST["contraseña"];
+			$correo = $_POST["correo"];
 
-		if (!isset($this->currentUser)) {
-			throw new Exception("Not in session. Editing users requires login");
-		}
-
-		$nombreusuario = $_REQUEST["nombreusuario"];
-		$user = $this->userMapper->findByName($nombreusuario);
-
-		if ($user == NULL) {
-			throw new Exception("no such exercise with username: ".$nombreusuario);
-		}
-
-
-		if (isset($_POST["submit"])) {
-
-			$user->setUsername($_POST["nombreusuario"]);
-			$user->setPassword($_POST["contraseña"]);
-			$user->setMail($_POST["correo"]);
-			$user->setUserType($_POST["tipousuario"]);
-
-
-			try {
-
-				$user->checkIsValidForUpdate();
-
-
-				$this->userMapper->update($user);
-
-
-				$this->view->setFlash(sprintf(i18n("Usuario \"%s\" actualizado."),$user ->getUsername()));
-
-
-				$this->view->redirect("users", "index");
-
-			}catch(ValidationException $ex) {
-
-				$errors = $ex->getErrors();
-
-				$this->view->setVariable("errors", $errors);
+		    if (isset($_GET['lang'])) {
+				$lang = $_GET['lang'];
+			} else {
+				$lang = "es";
 			}
+
+			$user = new User($nombreusuario,$nombre,$contraseña,$correo,"entrenador");
+
+			$userMapper->update($user,$nombreAntiguo);
+			header("Location: ../view/adminEntrenadores.php?lang=$lang");
 		}
-
-
-		$this->view->setVariable("user", $user);
-
-		$this->view->render("users", "edit");
-
-
-	}
 
 
 	public function delete() {
-		if (!isset($_POST["nombreusuario"])) {
-			throw new Exception("El nombre de usuario es obligatorio");
+		$nombreusuario = $_POST["nombreusuario"];
+		//$nombreusuario = $this->usuarioMapper->find($nombreusuario);
+		if (isset($_GET['lang'])) {
+			$lang = $_GET['lang'];
+		} else {
+			$lang = "es";
 		}
 
-		if (!isset($this->currentUser)) {
-			throw new Exception("Not in session. Editing tablas requires login");
-		}
+		$userMapper = new UserMapper();
+		$userMapper->delete($nombreusuario);
 
-		$nombreusuario = $_REQUEST["nombreusuario"];
-		$user = $this->userMapper->findByName($nombreusuario);
-
-		if ($user == NULL) {
-			throw new Exception("No se encuentra el nombre de usuario: ".$nombreusuario);
-		}
-
-
-
-
-		$this->userMapper->delete($user);
-
-
-		$this->view->setFlash(sprintf(i18n("Usuario \"%s\" eliminado."),$user ->getUsername()));
-
-
-		$this->view->redirect("users", "index");
-
+	    header("Location: ../view/adminEntrenadores.php?lang=$lang");
 	}
 }
